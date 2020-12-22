bb48a876b83c8eafadf0798dd899cd076df21e31
feliperiunk <manases9403@gmail.com>
2020-12-21 21:39:56 -0500

    upload Excel to json
    Se hizo una fucion de la rama de Esteban y se agrego la funcion para convertir un excel a json y de esa manera enviar datos a la api

--- app/api/leads/index.js
+++ app/api/leads/index.js
@@ -50,3 +50,65 @@ export const updateLeadStatus = async ({ leadId, status }) => {
     };
   }
 };
+
+export const ConvertExcelToJson = ({data})=>{
+  const excel = xlsx.readFile(url);
+  var nombreHoja = excel.SheetNames;
+  let datos = xlsx.utils.sheet_to_json(excel.Sheets[nombreHoja[0]]);
+// console.log(datos);
+const jdatos = [];
+for(i=0;i < datos.length; i++){
+   const dato = datos[i];
+   jdatos.push({
+       ...dato,
+       fecha: new Date((dato.fecha)),
+       nombre: dato.nombre,
+       edad: dato.edad,
+       email: dato.email,
+       telefono: dato.telefono,
+       compania: dato.compania,
+       tipo: dato.tipo,
+       mensaje: dato.mensaje,
+       estado: dato.estado,
+       ciudad: dato.ciudad,
+       municipio: dato.municipio,
+       ip: dato.ip,
+       estado_lead: dato.estado_lead,
+       asignacion: dato.asignacion,
+       observacion: dato.observacion,
+       status: dato.status,
+       compania: dato.compania_lead,
+       status_envio: dato.status_envio,
+       facebook: dato.facebook,
+       verificado: dato.verificado,
+       avatar: dato.avatar,
+       mail_marketing: dato.mail_marketing,
+       wa_send: dato.wa_send
+   });
+}
+return jdatos;
+};
+
+export const uploadExcelLeads = async({data})=>{
+  try {
+    const datos = ConvertExcelToJson(data);
+    const token = getJWTCrm();
+    return await axios.post(
+      `${config.hostname}/lead/excel`,
+      {
+        dato : datos
+      },
+      {
+        headers: {
+          Authorization: `Bearer: ${token}`,
+        },
+      }
+    );
+  } catch (err) {
+    console.log(err.response);
+    return {
+      status: err.response.data.payload.statusCode,
+      message: err.response.data.payload.message,
+    };
+  }
+};
